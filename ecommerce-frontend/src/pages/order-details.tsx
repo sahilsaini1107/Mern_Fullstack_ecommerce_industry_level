ee210723fef0a274c321df2e6190bbbd69826a44
Sahil <sohil170246@gmail.com>
2024-03-06 23:34:14 +0530

    admin dashboard is added to frontend

---
+++ ecommerce-frontend/src/pages/order-details.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const orderDetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default orderDetails
---
+++ ecommerce-frontend/src/pages/shipping.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const shipping = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default shipping
